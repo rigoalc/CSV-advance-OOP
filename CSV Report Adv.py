1f3d2a0e42b9e109c4b8e3aa2404852507a6c9ce
Rigo Alcover <rodigomartinalcover@gmail.com>
2021-12-12 21:07:42 -0500

    Update CSV Report Adv.py

--- CSV Report Adv.py
+++ CSV Report Adv.py
@@ -8,20 +8,10 @@
 from collections import Counter
 from typing import List
 
-class CSVReport:
-    
-    
-    
+class CSVReport:    
     def __init__(self):
-        self.reading = 'read_csv'
-        
-   
- 
-    def run(self):
-        
-        
-
-        
+        self.reading = 'read_csv' 
+    def run(self):       
         header = True  
         year_data = {}
         print("Year |  Min |  Max |  Avg") #header 
@@ -54,6 +44,45 @@ class CSVReport:
         
                 
             print("{} | {:=4} | {:>.1f} | {:>.1f}".format (year, minimun, maximun, average))#Print the results
+            
+class Monthly:    
+    def __init__(self):
+        self.reading = 'read_csv' 
+    def run(self, monthinput):       
+        header = True  
+        year_data = {}
+        print("Year |  Min |  Max |  Avg") #header 
+        print("==== | ==== | ==== | ====")
+        with open('MIGRNDRA.csv', 'r') as my_file:# Open file for reading with open(path)
+            for line in my_file.readlines():#search for lines 
+                line = line.strip() #   
+                if not line: 
+                    continue
+                if header:#search for header
+                    header = False#ignore the header
+                    continue
+                values = line.split(',')#How to read the values separated by a coma
+                month = int(values[0])#read and store values
+                day = int(values[1])
+                year = int(values[2])
+                temp = float(values[3])
+                if monthinput != month:
+                    continue
+                if year not in year_data:#load year 
+                    year_data[year] = []# add year to the collection
+                year_data[year].append(temp) #append the dictionary    
+      
+
+                continue        
+           
+        for year, temps in year_data.items():#read year and temps
+         
+            average = sum (temps) / len(temps)#calculate average
+            minimun = min (temps) #calulate minimun
+            maximun = max (temps)#calculate max
+        
+                
+            print("{} | {:=4} | {:>.1f} | {:>.1f}".format (year, minimun, maximun, average))#Print the results
 #How to read the values
 menu = (" 1) See breakdown by year","2) See breakdown by year for a single month","3) Quit")
         
@@ -68,8 +97,15 @@ if userinput == 1:
     my_csv_instance.run()
     
 elif userinput == 2:
-    print("Monthly")
     
+    monthinput = input("Please choose the month (1-12):")
+    monthinput = int(monthinput)
+     
+    my_csv_instance = Monthly()
+    my_csv_instance.run(monthinput)
+    
+    
+
 elif userinput == 3:
     print("Good Bye")
     
